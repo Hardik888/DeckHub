62e26a01b20d021a0154d04bcff7bb54616624f6
Hardik888 <hardik89990@gmail.com>
2024-03-25 13:45:39 +0530

    TESTING ENDPOINTS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,31 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
 config();
-import express, { Application,Request,Response } from "express";
+import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
+import DeckModel from "./models/Deck";
+const app: Application = express();
+app.use(express.json());
 
-const app : Application = express();
-
-
-mongoose.connect(process.env.MONGO_URL??
-    " ")
-.then(()=>{
-    console.log("listening to port ");
-    app.listen(process.env.PORT ?? " ");
-}).catch(error=>{
-    console.log(error)
-})
-
-
-
-
-app.get('/',(req: Request ,res:Response)=>{
-    res.send("hello world");
-    
+app.post('/decks', async (req: Request, res: Response) => {
+    const newDeck = new DeckModel({
+        title: req.body.title,
+        
+   
+    });
+    const createdDeck = await newDeck.save();
+    res.json(createdDeck);
 });
+
+
+mongoose.connect(process.env.MONGO_URL ?? " ")
+    .then(() => {
+        console.log("listening to port ");
+        app.listen(process.env.PORT ?? " ");
+    }).catch(error => {
+        console.log(error)
+    })
+
+
+
+
+
